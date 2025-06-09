e119839740ed9e6a5d09b0f75ef1573f08aa9114
Bhavy <dbhavy03@gmail.com>
2025-06-09 13:33:19 -0400

    Add files via upload

---
+++ scipt2.js
@@ -0,0 +1,223 @@
+let dog = 7
+let dogp = prompt(`Enter your dog's age:`)
+console.log(dogp)
+if (dog < dogp) {
+    console.log(`The dog is older than 7`)
+} else {
+    console.log(`the dog is not older than 7`)
+}
+/// prompt used as input
+/// let or const use to make variable 
+/// if else used as it when comparing using () and after using {}
+/// console.log to print
+
+
+let temp = 30
+let tempp = prompt(`Enter the Temperature just enter the first two number example 50 `)
+console.log(tempp)
+if (temp < 40) {
+    console.log(`It's hot`)
+} else {
+    console.log(`Its cold `)
+}
+
+let gameScore = 100
+const usersScore = prompt(`Enter your game score `)
+if (gameScore <= usersScore) {
+    console.log(`You got a high score`)
+} else {
+    console.log(`Good score`)
+}
+
+let candies = 10
+let candiesYouHave = prompt(`How many cndies do you have `)
+if (candies = candiesYouHave ) {
+    console.log(`You have ${candiesYouHave} candies that  should be ${candies}`)
+} else {
+    console.log(`you have ${candies} candies `)
+}
+
+
+let heightLimt = 48 
+let yourHeight = prompt(`Enter your height `)
+if (heightLimt <= yourHeight) {
+    console.log(`You are tall enough to ride `)
+} else {
+    console.log(`You are not tall enough to ride be 48 or more to ride`)
+}
+
+let booksLimit = 5
+let booksYouRead = prompt(`How many books have you read `)
+if (booksLimit < booksYouRead) {
+    console.log(`Reading Champ!`)
+} else {
+    console.log(`Read more to be the Reading Champ! read ${booksLimit - booksYouRead} more books to be the champ!`)
+}
+
+let homeWorkLimit = 30 
+let howmworkYouDid = prompt(`How many minutes of homeWork did you do `)
+if (homeWorkLimit > howmworkYouDid) {
+    console.log(`You have did ${homeWorkLimit - howmworkYouDid} minutes of homework`)
+} else {
+    console.log(`you have did ${howmworkYouDid - homeWorkLimit} minutes of more homwwork`)
+}
+
+
+let sibblings = 3 
+let sibblingYouHave =  prompt(`Enter how many Sibblings you have `)
+if (sibblings < sibblingYouHave) {
+    console.log(`You have a big Family `)
+} else {
+    console.log(`You have a small family`)
+}
+
+
+let time = 12
+let yourTime  = Prompt(`What time is it over there Just the 2 digits`)
+if (time > yourTime) {
+    console.log(`Good Morning!`)
+} else {
+    console.log(`Good Afternoon!`)
+}
+
+
+let moneyLimit = 10 
+let moneyYouHave = prompt(`How much money allowence do you have `)
+if (moneyLimit >= moneyYouHave) {
+    console.log(`You need more money allowence!`)
+} else {
+    console.log(`You got The right money allowence`) 
+}
+
+let pets = 2
+let petYouHave
+if (pets < petYouHave) {
+    console.log(`Animal Lover!`)
+} else {
+    console.log(`You got ${petYouHave} pets`)
+}
+
+
+let texts = 50
+let textsYouSent = prompt(`how many texts have you sent`)
+if (texts < textsYouSent) {
+    console.log(`Whoa! Too many texts!`)
+} else {
+    console.log(`you have sent ${textsYouSent} texts`)
+}
+
+
+
+
+
+
+
+let gameLevel = 10
+let yourGameLevel = prompt(`What is your game level!`) 
+if (gameLevel <= yourGameLevel) {
+    console.log(`Cool`)
+} else {
+    console.log(`your game level is ${yourGameLevel}`)
+}
+
+let pianoPracticeLimit = 3
+let yourPianoPractice = prompt(`For how many years have you been practicing piano. ex 3yrs type 3.`)
+if (pianoPracticeLimit <= yourPianoPractice) {
+    console.log(`Piano pro`)
+} else {
+    console.log(`Good job!`)
+}
+
+
+let pencilsYouShouldHave = 3
+let howAManyPencilesYouHave = prompt(`How many pencils do yoou have `)
+if (pencilsYouShouldHave >= howAManyPencilesYouHave) {
+    console.log(`Not enough`)
+} else {
+    console.log(`enough Pencils`)
+}
+
+let study = 65
+let marks = prompt(`What score did you got in your SOL's type from 1-100`)
+if (study <= marks) {
+    console.log(`you failed. study more!`)
+} if (marks = +100) {
+    console.log(`invalid value`)
+} else {
+    console.log(`you passed ${marks}`)
+}
+
+let age = 11
+let usersAge = prompt(`what's your age `)
+if (age = usersAge) {
+    console.log(`your age and my age Matches `)
+} else {
+    console.log(`your age is ${usersAge}`)
+}
+
+
+let pushUps = 20
+let yourPushUps = prompt(`How many pushups did you do `)
+if (pushUps <= yourPushUps) {
+    console.log(`Strong!`)
+} else {
+    console.log(`Keep practicing`)
+}
+
+
+let sleep = 8
+let yourSleep = prompt(`how many hourss of sleep did you get`)
+
+
+if (sleep > yourSleep) {
+    console.log(`you need More sleep!`)
+} else {
+    console.log(`Enough Sleep`)
+}
+
+
+let steps = 10000
+let yourSteps = prompt(`How many steps have you walked`)
+if (steps <= yourSteps) {
+    console.log(`You have completed your 10,000 steps goal.`)
+} else {
+    console.log(`keep walking ${steps - yourSteps} step more to complete the goal`)
+}
+
+let batrey = 20
+let yourBatrey = prompt(`how much is your batray persentage`)
+if (batrey > yourBatrey) {
+    console.log(`Charge your phone`)
+} else {
+    console.log(`enoigh batery`)
+}
+
+let videos = 5
+let videosYouWatched = prompt(`How many youtube videos did you watch`)
+if (videos <= videosYouWatched) {
+    console.log(`Screen-Time Break!`)
+} else {
+    console.log(``)
+}
+
+let aples = 0
+
+if (aples = 0) {
+    console.log(`out of Apples!`)
+} else {
+    console.log(``)
+}
+
+let days = 10 
+if (days > 9) {
+    console.log(`Almost party time `)
+} else {
+    console.log(``)
+}
+
+let icecreemScopes = 3
+if (icecreemScopes < 4) {
+    console.log(`sugar overload!`)
+} else {
+    console.log(``)
+}
